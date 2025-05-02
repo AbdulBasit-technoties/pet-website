c968ca99a851460fe81ff806370f526cbc0b7496
wariswaseem <160909665+msaqib124@users.noreply.github.com>
2025-05-02 22:55:14 +0500

    updates

---
+++ resources/js/FrontendComponents/PetPassport/PetPassport.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PetPassport() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- resources/js/Pages/Frontend/Home.jsx
+++ resources/js/Pages/Frontend/Home.jsx
@@ -1,10 +1,28 @@
+import Banner from '@/FrontendComponents/Banner/Banner'
+import Footer from '@/FrontendComponents/Footer/Footer'
 import Header from '@/FrontendComponents/Header/Header'
+import OurServices from '@/FrontendComponents/OurServices/OurServices'
+import OurTeam from '@/FrontendComponents/OurTeam/OurTeam'
+import PetFriendly from '@/FrontendComponents/PetFriendly/PetFriendly'
+import PetPassport from '@/FrontendComponents/PetPassport/PetPassport'
+import Testimonial from '@/FrontendComponents/Testimonial/Testimonial'
+import WeCare from '@/FrontendComponents/WeCare/WeCare'
+import WhyChoose from '@/FrontendComponents/WhyChoose/WhyChoose'
 import React, { Fragment } from 'react'
 
 export default function Home() {
     return (
         <Fragment>
             <Header />
+            <Banner />
+            <OurServices />
+            <PetFriendly />
+            <WhyChoose />
+            <PetPassport />
+            <OurTeam />
+            <WeCare />
+            <Testimonial />
+            <Footer />
         </Fragment>
     )
 }
