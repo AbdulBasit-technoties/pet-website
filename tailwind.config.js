2fa69fc0d34e73281440280eabd68c1e0ca32d96
AbdulBasit-technoties <abasit.technotie@gmail.com>
2025-05-02 22:25:44 +0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Lato", ...defaultTheme.fontFamily.sans],
+                heading: ["Montserrat", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                c1: 'rgba(63, 155, 12, 1)',     
+                c2: 'rgba(209, 229, 8, 1)',     
+                c3: 'rgba(113, 113, 113, 1)',    
+                c4: 'rgba(255, 255, 255, 1)',     
+                custbg: 'rgba(246, 244, 244, 1)',   
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
